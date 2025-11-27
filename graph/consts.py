24b3e24fcae0af4ce0f34cd45497c338a98346c4
ousama lasri <oussama.lasri.2k@gmail.com>
2025-11-27 22:02:35 +0100

    DEFINE CONSTANTS TO AVOID HARD-CODING STRINGS MULTIPLE TIMES

---
+++ graph/consts.py
@@ -0,0 +1,12 @@
+# WE DEFINE CONSTANTS FOR DIFFERENT CHAIN TYPES FOR EASY REFERENCE THROUGHOUT THE CODEBASE.
+# AND TO AVOID HARD-CODING STRINGS MULTIPLE TIMES.
+
+RETRIEVE = "retrieve"
+GENERATE = "generate"
+GRADEDOCUMENTS = "grade_documents"
+WEB_SEARCH = "web_search"
+
+#     "generate_answer",
+#     "retriver",
+#     "web_search",
+#     "grade_documents",
---
+++ graph/nodes/__init__.py
@@ -0,0 +1,11 @@
+from graph.nodes.generate import generate_answer
+from graph.nodes.retrieve import retriver
+from graph.nodes.web_search import web_search
+from graph.nodes.grade_documents import grade_documents
+
+__all__ = [
+    "generate_answer",
+    "retriver",
+    "web_search",
+    "grade_documents",
+]
